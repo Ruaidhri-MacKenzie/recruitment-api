72231efca04af81f736eed5d6d2e44c926b56e8c
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2020-02-07 12:15:04 +0000

    Added checkAuth middleware and authenticate route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const checkAuth = require('../middleware/checkAuth.js');
 const router = express.Router();
 
 const userController = require('../controllers/userController.js');
 
-router.get('/', userController.readAllUsers);
+router.get('/', checkAuth, userController.readAllUsers);
 router.post('/', userController.createUser);
 
-router.get('/:id', userController.readUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/:id', checkAuth, userController.readUser);
+router.put('/:id', checkAuth, userController.updateUser);
+router.delete('/:id', checkAuth, userController.deleteUser);
+
+router.post('/:id', userController.authenticate);
 
 module.exports = router;
