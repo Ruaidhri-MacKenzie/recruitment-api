1ae6b6bb04204012d38cacfb0f33efe6b2a19ac3
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2020-02-07 12:15:31 +0000

    Added password and verified fields to User schema

--- models/User.js
+++ models/User.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-	name: { type: String, required: true },
+	username: { type: String, required: true },
+	password: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
-	phone: { type: String },
+	verified: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', UserSchema, 'users');
