b339afa98321ca44c61cc13b62c857eaebde2d04
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2020-02-07 12:16:39 +0000

    Changed from importing from keys file to using environment variables

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const keys = require('./keys.js');
 
 const userRoutes = require('./routes/userRoutes.js');
 
 const app = express();
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/user', userRoutes);
 
-mongoose.connect(`mongodb://${keys.mongo.user}:${keys.mongo.pass}@ds215219.mlab.com:15219/recruitment`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds215219.mlab.com:15219/recruitment`, { useNewUrlParser: true, useUnifiedTopology: true });
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
